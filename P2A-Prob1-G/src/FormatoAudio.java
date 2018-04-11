417d28a13f013687c560fe58b8bb86496e8e2eda
wvschmitz <wellington.vossschmitz@gmail.com>
2018-04-10 21:20:19 -0300

    Criado a interface FormatoAudio

--- P2A-Prob1-G/src/FormatoAudio.java
+++ P2A-Prob1-G/src/FormatoAudio.java
@@ -1,3 +1,20 @@
 public interface FormatoAudio {
+
+    public void abrir(String file);
+
+    public void reproduzir();
+
+    public void pausar();
+
+    public void parar();
+
+    public void avancar(int value);
+
+    public void retornar(int value);
+
+    public void liberar();
+
+    public void reproduzirSimples(String file);
     
+    public void pararSimples();
 }
