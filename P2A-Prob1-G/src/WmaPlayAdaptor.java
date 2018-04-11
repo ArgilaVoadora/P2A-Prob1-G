2d32f53d84d11056a4a8c9eca6c383e46b317b9a
Guilherme Konell <eu.gui.konell@gmail.com>
2018-04-10 21:46:12 -0300

    implementação da interface

---
+++ P2A-Prob1-G/src/WmaPlayAdaptor.java
@@ -0,0 +1,55 @@
+
+import problema1.wmaPlay;
+
+public class WmaPlayAdaptor implements FormatoAudio {
+    
+    wmaPlay arquivo;
+    
+    @Override
+    public void abrir(String file) {
+        arquivo.setFile(file);
+    }
+
+    @Override
+    public void reproduzir() {
+        arquivo.play();
+    }
+
+    @Override
+    public void pausar() {
+        arquivo.stop();
+    }
+
+    @Override
+    public void parar() {
+        arquivo.setLocation(0);
+    }
+
+    @Override
+    public void avancar(int value) {
+        arquivo.setLocation(arquivo.getLocation() + value);
+    }
+
+    @Override
+    public void retornar(int value) {
+        arquivo.setLocation(arquivo.getLocation() - value);
+    }
+
+    @Override
+    public void liberar() {
+        arquivo = null;
+    }
+
+    @Override
+    public void reproduzirSimples(String file) {
+        abrir(file);
+        reproduzir();
+    }
+
+    @Override
+    public void pararSimples() {
+        parar();
+        liberar();
+    }
+    
+}
